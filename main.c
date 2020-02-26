fef6f04a421dcbe388dbef2d1b4f0819a15fd5d5
Apolline CHARPENTIER <apcharpe@e1r1p6.42.fr>
2020-02-26 10:58:25 +0100

    git

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: apcharpe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 15:42:48 by apcharpe          #+#    #+#             */
-/*   Updated: 2020/02/26 10:53:48 by apcharpe         ###   ########.fr       */
+/*   Updated: 2020/02/26 10:57:52 by apcharpe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(void)
 	int		i;
 
 //	ac = ac + 0;
-	fd = open("toto3.txt", O_RDONLY);
+	fd = open("toto.txt", O_RDONLY);
 	while ((i = get_next_line(fd, &line)) > 0)
 	{
 		printf("[%d] | %s\n", i, line);
@@ -29,6 +29,6 @@ int	main(void)
 		get_next_line(fd, &line);
 		printf("[%d] | %s\n", i, line);
 		close (fd);
-//		system("leaks a.out");
+		system("leaks a.out");
 		return (0);
 }
